83a75d85efa36ae072e6ef077a260a72e6b7a988
raihana <zakiaraihanauni@gmail.com>
2025-09-15 17:06:49 +0700

    edit settings.py

--- football_shop_tugas2/settings.py
+++ football_shop_tugas2/settings.py
@@ -33,6 +33,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raihana-auni-footballshoptugas2.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://raihana-auni-footballnews.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
